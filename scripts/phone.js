afac3c1a9e24175bca392891c97b0100e0e9b4bd
fahimnirjon <fahimnirjon21@gmail.com>
2024-05-14 00:48:13 +0600

    various changes made

--- scripts/phone.js
+++ scripts/phone.js
@@ -2,7 +2,7 @@ const loadPhone = async (searchText) =>{
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
-    // console.log(phones);
+    console.log(phones);
     displayPhones(phones);
 }
 
@@ -15,7 +15,7 @@ const displayPhones = phones =>{
 // display show all
     const showAll = document.getElementById('show-button');
 
-    if(phones.length > 10){
+    if(phones.length > 20){
         showAll.classList.remove('hidden')
     }
     else{
@@ -37,20 +37,68 @@ const displayPhones = phones =>{
         <h2 class="card-title">${phone.phone_name}</h2>
         <p>${phone.slug}</p>
         <div class="card-actions">
-          <button class="btn btn-primary">Buy Now</button>
+          <button onclick="showDetail('${phone.slug}');my_modal_5.showModal()" class="btn btn-primary">Show Details</button>
         </div>
       </div>
         `;
+        // append child
         phoneContainer.appendChild(phoneCard);
-    })
+    });
 
+    // hide loading spinner
+    loadingSpinner(false);
+}
+
+const showDetail =async(id) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+   const phoneData = data.data;
+    showPhoneDeatils(phoneData);
 }
 
 // search button
 
 const handleSearch = () => {
+    loadingSpinner(true);
    const searchField = document.getElementById('search-handler');
    const searchText = searchField.value;
    console.log(searchText);
    loadPhone(searchText);
+}
+
+const loadingSpinner = (isTrue) =>{
+      const load = document.getElementById('spin-loadding'); 
+      if(isTrue){
+        load.classList.remove('hidden'); 
+      }
+       else{
+        load.classList.add('hidden');
+       }
+}
+
+const showPhoneDeatils = (phoneData) =>{
+
+    const phoneName = document.getElementById('phone-name');
+    phoneName.innerText = phoneData.name;
+
+
+    my_modal_5.showModal();
+    console.log(phoneData)
+
+    const showAllContainer = document.getElementById('all-container');
+    showAllContainer.innerHTML = `
+        
+        <img src="${phoneData.image}" alt=""/>
+        <p><span>Brand:</span>${phoneData.brand}</p>
+        <p><span>Chipset:</span>${phoneData.mainFeatures.chipSet}</p>
+        <p><span>Display Size:</span>${phoneData.mainFeatures.displaySize}</p>
+        <p><span>Memory:</span>${phoneData.mainFeatures.memory}</p>
+        <p><span>Storage:</span>${phoneData.mainFeatures.storage}</p>
+        <p><span>Sensors:</span>${phoneData.mainFeatures.sensors}</p>
+        <p><span>Others:</span>${phoneData.others.Bluetooth}</p>
+        <p><span>Release Date:</span>${phoneData.
+            releaseDate}</p>
+    `
+
+
 }
